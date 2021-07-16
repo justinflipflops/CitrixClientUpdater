07d1ae9fe4f24462bf746ba15c646ee46544e311
Justin Oberdorf <justin.oberdorf@gmail.com>
2021-07-16 15:51:43 -0400

    Add project files.

---
+++ CitrixClientUpdater/CitrixVersionChecker.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Common;
+using AngleSharp.Dom;
+
+namespace CitrixClientUpdater
+{
+    public class CitrixVersionChecker
+    {
+        const string internal_CitrixWindowsURL = @"https://www.citrix.com/downloads/workspace-app/windows/workspace-app-for-windows-latest.html";
+        const string internal_CitrixMacURL = @"https://www.citrix.com/downloads/workspace-app/mac/workspace-app-for-mac-latest.html";
+        private CitrixClientOperatingSystem OperatingSystem { get; }
+        public CitrixVersionChecker(CitrixClientOperatingSystem OperatingSystem)
+        {
+            this.OperatingSystem = OperatingSystem;
+        }
+        public async Task<CitrixVersionInformation> GetLatestVersionInformation()
+        {
+            CitrixVersionInformation _CVI = new CitrixVersionInformation();
+            const string internal_CtxTextContent = "ctx-text-content";
+            const string internal_NameSelector = "body>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(3)>div>h1";
+            const string internal_ReleaseDateSelector = "body>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(3)>div>h3";
+            const string internal_DownloadLinkSelector = "body>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(3)>div>div:nth-child(4)>div:nth-child(2)>div>div>div>div>a";
+            const string internal_VersionSelector = "body>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div:nth-child(1)>div:nth-child(3)>div>p:nth-child(2)";
+            const string internal_WindowsChecksumSelector = "body>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div:nth-child(1)>div:nth-child(3)>div>div";
+            const string internal_MacOSChecksumSelector = "body>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div:nth-child(1)>div:nth-child(3)>div>p:nth-child(3)";
+            string _citrixUpdateURL = this.OperatingSystem == CitrixClientOperatingSystem.WINDOWS ? internal_CitrixWindowsURL : internal_CitrixMacURL;
+            IConfiguration angle_config = Configuration.Default.WithDefaultLoader();
+            using (IBrowsingContext angle_browsingContext = BrowsingContext.New(angle_config))
+            {
+                try
+                {
+                    IDocument angle_document = await angle_browsingContext.OpenAsync(_citrixUpdateURL);
+                    _CVI.Name = angle_document.QuerySelector(internal_NameSelector).TextContent.Trim();
+                    _CVI.ReleaseDate = angle_document.QuerySelector(internal_ReleaseDateSelector).TextContent.Replace("Release Date:", "").Trim();
+                    _CVI.Version = angle_document.QuerySelector(internal_VersionSelector).TextContent.Replace("Version:", "").Trim();
+                    _CVI.DownloadURL = "https:" + angle_document.QuerySelector(internal_DownloadLinkSelector).GetAttribute("rel");
+                    _CVI.FileName = Path.GetFileName(_CVI.DownloadURL.Split("?")[0]);
+                    string _checksum = angle_document.QuerySelector(this.OperatingSystem == CitrixClientOperatingSystem.WINDOWS ? internal_WindowsChecksumSelector : internal_MacOSChecksumSelector).TextContent;
+                    _checksum = this.OperatingSystem == CitrixClientOperatingSystem.WINDOWS ? _checksum.Replace("-", "").Replace(" ", "").Replace("\xA0","").ToUpperInvariant().Trim() : _checksum.Replace("Checksums\n", "").Replace("-", "").Replace(" ", "").Replace("\xA0", "").ToUpperInvariant().Trim();
+                    if (_checksum.StartsWith("SHA"))
+                    {
+                        if (_checksum.StartsWith("SHA256"))
+                        {
+                            _CVI.ChecksumType = CitrixChecksumType.SHA256;
+                            _CVI.Checksum = _checksum.Substring(6);
+                        }else if (_checksum.StartsWith("SHA1"))
+                        {
+                            _CVI.ChecksumType = CitrixChecksumType.SHA1;
+                            _CVI.Checksum = _checksum.Substring(4);
+                        }
+                        else
+                        {
+                            _CVI.ChecksumType = CitrixChecksumType.UNKNOWN;
+                            _CVI.Checksum = "";
+                        }
+                    }
+                    return _CVI;
+                }
+                catch (Exception _ex)
+                {
+                    _ex = _ex;
+                }
+            }
+            return null;
+        }
+    }
+    public class CitrixVersionInformation
+    {
+        public string Name { get; set; }
+        public string FileName { get; set; }
+        public string ReleaseDate { get; set; }
+        public string DownloadURL { get; set; }
+        public string Version { get; set; }
+        public string Checksum { get; set; }
+        public CitrixChecksumType ChecksumType { get; set; }
+    }
+    public enum CitrixChecksumType
+    {
+        SHA1,
+        SHA256,
+        UNKNOWN
+    }
+    public enum CitrixClientOperatingSystem
+    {
+        WINDOWS,
+        MACOS
+    }
+}
---
+++ CitrixClientUpdater/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Net.Http;
+
+namespace CitrixClientUpdater
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            const string interal_RootPath = @"C:\Program Files\Citrix\Receiver StoreFront\Receiver Clients\";
+            if (Directory.Exists(interal_RootPath))
+            {
+                Console.WriteLine("[-] Citrix StoreFront Receiver Clients Root Path Found");
+                CitrixVersionInformation win_Version = null;
+                CitrixVersionInformation mac_Version = null;
+                try
+                {
+                    Console.WriteLine("[-] Checking Citrix for current Windows version information.");
+                    CitrixVersionChecker win_CSC = new CitrixVersionChecker(CitrixClientOperatingSystem.WINDOWS);
+                    win_Version = await win_CSC.GetLatestVersionInformation();
+                    Console.WriteLine($"[-] Latest Citrix for current Windows version: {win_Version.Version}");
+                }
+                catch
+                {
+                    Console.WriteLine("[ERROR] Checking Citrix for current Windows version information.");
+                    return;
+                }
+                try
+                {
+                    Console.WriteLine("[-] Checking Citrix for current MacOS version information.");
+                    CitrixVersionChecker mac_CSC = new CitrixVersionChecker(CitrixClientOperatingSystem.MACOS);
+                    mac_Version = await mac_CSC.GetLatestVersionInformation();
+                    Console.WriteLine($"[-] Latest Citrix for current MacOS version: {mac_Version.Version}");
+                }
+                catch
+                {
+                    Console.WriteLine("[ERROR] Checking Citrix for current MacOS version information.");
+                    return;
+                }
+                bool win_clientForceUpdate = false;
+                bool mac_clientForceUpdate = false;
+                string win_clientPath = interal_RootPath + win_Version.FileName;
+                string mac_clientPath = interal_RootPath + mac_Version.FileName;
+                if (File.Exists(win_clientPath))
+                {
+                    Console.WriteLine("[-] Found existing Citrix client for Windows. Checking checksum.");
+                    string win_currentChecksum = await GetChecksumBuffered(win_Version.ChecksumType, win_clientPath);
+                    if (win_currentChecksum != win_Version.Checksum)
+                    {
+                        Console.WriteLine("[-] Citrix client for Windows Checksum mismatch. Removing. Flagging for Update.");
+                        File.Delete(win_clientPath);
+                        win_clientForceUpdate = true;
+                    }
+                    else
+                        Console.WriteLine("[-] Citrix Client for Windows Checksum match. Up to Date. Skipping.");
+                }
+                else
+                {
+                    Console.WriteLine("[-] Existing Citrix client for Windows NOT FOUND. Flagging for Download.");
+                    win_clientForceUpdate = true;
+                }
+                if (File.Exists(mac_clientPath))
+                {
+                    Console.WriteLine("[-] Found existing Citrix client for MacOS. Checking checksum.");
+                    string mac_currentChecksum = await GetChecksumBuffered(mac_Version.ChecksumType, mac_clientPath);
+                    if (mac_currentChecksum != mac_Version.Checksum)
+                    {
+                        Console.WriteLine("[-] Citrix client for MacOS Checksum mismatch. Removing. Flagging for Update.");
+                        File.Delete(mac_clientPath);
+                        mac_clientForceUpdate = true;
+                    }
+                    else
+                        Console.WriteLine("[-] Citrix Client for MacOS Checksum match. Up to Date. Skipping.");
+                }
+                else
+                {
+                    Console.WriteLine("[-] Existing Citrix client for MacOS NOT FOUND. Flagging for Download.");
+                    mac_clientForceUpdate = true;
+                }
+                if (win_clientForceUpdate)
+                {
+                    Console.WriteLine("[-] Updating Citrix client for Windows.");
+                    try
+                    {
+                        using (HttpClient win_downloadClient = new HttpClient())
+                        {
+                            using (Stream web_Stream = await win_downloadClient.GetStreamAsync(win_Version.DownloadURL))
+                            {
+                                using (Stream file_Stream = File.OpenWrite(win_clientPath))
+                                {
+                                    await web_Stream.CopyToAsync(file_Stream);
+                                    await file_Stream.FlushAsync();
+                                    file_Stream.Close();
+                                    Console.WriteLine("[-] Downloaded Citrix client for Windows. Calculating Checksum.");
+                                    string win_downloadChecksum = await GetChecksumBuffered(win_Version.ChecksumType, win_clientPath);
+                                    if (win_downloadChecksum != win_Version.Checksum)
+                                    {
+                                        Console.WriteLine("[ERROR] Citrix client for Windows Checksum mismatch. Removing Download.");
+                                        File.Delete(win_clientPath);
+                                    }
+                                    else
+                                        Console.WriteLine("[-] Citrix Client for Windows Checksum match.");
+                                }
+                                web_Stream.Close();
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("[ERROR] Updating Citrix client for Windows.");
+                    }
+                }
+                else
+                    Console.WriteLine("[-] Skipping Citrix Client for Windows.");
+                if (mac_clientForceUpdate)
+                {
+                    Console.WriteLine("[-] Updating Citrix client for MacOS.");
+                    try
+                    {
+                        using (HttpClient mac_downloadClient = new HttpClient())
+                        {
+                            using (Stream web_Stream = await mac_downloadClient.GetStreamAsync(mac_Version.DownloadURL))
+                            {
+                                using (Stream file_Stream = File.OpenWrite(mac_clientPath))
+                                {
+                                    await web_Stream.CopyToAsync(file_Stream);
+                                    await file_Stream.FlushAsync();
+                                    file_Stream.Close();
+                                    Console.WriteLine("[-] Downloaded Citrix client for MacOS. Calculating Checksum.");
+                                    string mac_downloadChecksum = await GetChecksumBuffered(mac_Version.ChecksumType, mac_clientPath);
+                                    if (mac_downloadChecksum != mac_Version.Checksum)
+                                    {
+                                        Console.WriteLine("[ERROR] Citrix client for MacOS Checksum mismatch. Removing Download.");
+                                        File.Delete(mac_clientPath);
+                                    }
+                                    else
+                                        Console.WriteLine("[-] Citrix Client for MacOS Checksum match.");
+                                }
+                                web_Stream.Close();
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("[ERROR] Updating Citrix client for MacOS.");
+                    }
+                }
+                else
+                    Console.WriteLine("[-] Skipping Citrix Client for MacOS.");
+            }
+            else
+                Console.WriteLine("[ERROR] Citrix Receiver Clients RootPath does not exist. Is this being run on a Citrix StoreFront server?");
+            return;
+        }
+        public static async Task<string> GetChecksumBuffered(CitrixChecksumType Type, String FilePath)
+        {
+            if (File.Exists(FilePath))
+            {
+                using (var InputStream = File.OpenRead(FilePath))
+                {
+                    using (var bufferedStream = new BufferedStream(InputStream, 1024 * 32))
+                    {
+                        switch (Type)
+                        {
+                            case CitrixChecksumType.SHA256:
+                                SHA256 sha256 = SHA256.Create();
+                                byte[] checksum256 = await sha256.ComputeHashAsync(bufferedStream);
+                                return BitConverter.ToString(checksum256).Replace("-", String.Empty).ToUpperInvariant();
+                            case CitrixChecksumType.SHA1:
+                                SHA1 sha1 = SHA1.Create();
+                                byte[] checksum1 = await sha1.ComputeHashAsync(bufferedStream);
+                                return BitConverter.ToString(checksum1).Replace("-", String.Empty).ToUpperInvariant();
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
